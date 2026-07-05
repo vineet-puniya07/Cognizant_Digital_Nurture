07394d41bdc8faccf0df6636bcfb423700506616
Vinit kumar <vineetpuniya392@gmail.com>
2026-07-05 15:51:37 +0530

    [Vinit] add:  SpringCoreAndMaven Exercise 1 Completed

---
+++ WEEK2/SpringCoreAndMaven/LibraryManagement/src/main/java/com/library/MainApp.java
@@ -0,0 +1,24 @@
+package com.library;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        BookRepository repository =
+                context.getBean("bookRepository", BookRepository.class);
+
+        service.displayService();
+        repository.displayRepository();
+    }
+}
