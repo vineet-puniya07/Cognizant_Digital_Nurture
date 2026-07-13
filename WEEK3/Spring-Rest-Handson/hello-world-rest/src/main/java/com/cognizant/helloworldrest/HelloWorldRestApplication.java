9088664f14337924ce570cf90bfc1a74eadcde92
Vinit kumar <vineetpuniya392@gmail.com>
2026-07-13 21:30:37 +0530

    [Vinit] add:  Completed Hello World RESTful Web Services exercise

---
+++ WEEK3/Spring-Rest-Handson/hello-world-rest/src/main/java/com/cognizant/helloworldrest/HelloWorldRestApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.helloworldrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestApplication.class, args);
+	}
+
+}
